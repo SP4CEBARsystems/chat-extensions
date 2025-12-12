46d7ca76564349fc674f7dc44472ada5cb9c5d6d
Boaz Crezee <bjcrezee@gmail.com>
2025-12-12 16:52:25 +0100

    added a js json blob generator and a query redirector to this blob

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Hello");
+    generateSVG();
+});
+
+function generateSVG() {
+    // Generate SVG string
+    const svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="80">
+        <text x="10" y="40" font-size="24">Hello!</text>
+    </svg>
+    `;
+
+    // Create a blob URL
+    const blob = new Blob([svg], { type: "image/svg+xml" });
+    const url = URL.createObjectURL(blob);
+
+    // Use it
+    // document.querySelector("img").src = url;
+    // or open in new tab
+    // window.open(url, "_blank");
+
+    if (new URLSearchParams(window.location.search).get("image") === "0") {
+        window.location.href = url;
+    }
+}
